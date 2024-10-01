3ce50c7a896555e82b1ea0dc89b110bb382d8eaa
Severino <severin.opel@hotmail.com>
2024-10-01 17:45:31 +0200

    added base path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+  base: '/spacialist-docs/',
   lang: 'en-US',
 
   title: 'Spacialist Documentation',
